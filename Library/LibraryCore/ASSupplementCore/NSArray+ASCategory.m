86e615d72fcd4aeb1645d1fc99a74327a7d6b8ef
JayceYang <Jayce.Yang@me.com>
2013-03-09 15:33:06 +0800

    Update NSArrey category.

--- Library/LibraryCore/ASSupplementCore/NSArray+ASCategory.m
+++ Library/LibraryCore/ASSupplementCore/NSArray+ASCategory.m
@@ -18,13 +18,16 @@
     return [self sortedArrayUsingDescriptors:sortDescriptors];
 }
 
-- (id)objectAtTheIndex:(NSUInteger)index
+- (id)objectAtTheIndex:(NSInteger)index
 {
     if (![self isKindOfClass:[NSArray class]]) {
         ASLog(@"%@ is not array type", NSStringFromClass([self class]));
         return nil;
     } else if (index >= self.count) {
         ASLog(@"index (%d) beyond bounds (%d)", index, self.count);
+        return nil;
+    }  else if (index < 0) {
+        ASLog(@"index (%d) is invalid", index);
         return nil;
     } else {
         return [self objectAtIndex:index];
@@ -34,6 +37,11 @@
 - (id)firstObject
 {
     return [self objectAtTheIndex:0];
+}
+
+- (id)theLastObject
+{
+    return [self objectAtTheIndex:self.count - 1];
 }
 
 @end
